db37d2389024dd0fc5871018f8c86b4010c80527
Zalesskaya Kristina <mrskotikk@gmail.com>
2025-11-19 09:28:53 +0300

    Check connection

---
+++ user-service/src/main/java/TestDBConnection.java
@@ -0,0 +1,17 @@
+/*import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestDBConnection {
+    public static void main(String[] args) {
+        String url = "jdbc:postgresql://localhost:5433/user_db";
+        String user = "user";
+        String password = "your_pass";
+
+        try (Connection connection = DriverManager.getConnection(url, user, password)) {
+            System.out.println("Connected to the database!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}*/
+
